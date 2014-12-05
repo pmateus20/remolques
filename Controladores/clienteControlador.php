75b4d15fcf02ae835ea1adf7f9ecb5a06d28194f
pmateus20 <pmateus20@gmail.com>
2014-12-04 21:21:15 -0300

    ejemplo get y set

--- Controladores/clienteControlador.php
+++ Controladores/clienteControlador.php
@@ -1,12 +1,30 @@
 <?php 
 
 class clienteControlador extends controlador{
+	private $idCliente;
+	private $apellidoNombre;
+
+
 	function crear(){
 		$this->vista->render("cliente/crear");
 	}
 	function listar(){
 		$this->vista->render("cliente/listar");
 	}
+
+
+	//get y set
+	function getapellidoNombre(){
+		return this->apellidoNombre;
+	}
+
+	function setapellidoNombre($id){
+		this->apellidoNombre=$id;
+	}
+
+
+	//fin get y set
+
 
 }
 
